20afd62b6b96417d691134c4a3e35dc3f4d73386
Fisto123 <oluwafisayoiyiola@gmail.com>
2023-02-28 22:54:57 +0100

    new stuffs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
 };
-=======
-  images:{
-    loader:'akamai',
-    path:'/',
-  }
-}
->>>>>>> 6627c605ae8cf5a258403ce34762a6f44d2b3a4f
 
 module.exports = nextConfig;
